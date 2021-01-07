1a4c0e18153107626483a4b3568aebfbf0007e62
Marvellous chimaraoke <rokemarvellous@gmail.com>
2021-01-07 19:15:41 +0100

    Added blog and jobs models and migrated to postgres database

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models.fields import CharField
 
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    publish_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
+
