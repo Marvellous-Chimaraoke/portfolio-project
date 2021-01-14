f1e329841680f49533a445ac5337faa48d5aa59d
Marvellous chimaraoke <rokemarvellous@gmail.com>
2021-01-14 15:20:12 +0100

    Added URL, title to jobs model..fixed view button

--- jobs/models.py
+++ jobs/models.py
@@ -4,5 +4,9 @@ from django.db import models
 
 class Job(models.Model):
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, default=0)
+    summary = models.CharField(max_length=400)
+    URL = models.URLField(max_length=200, default=0)
     
+    def __str__(self):
+        return self.title
