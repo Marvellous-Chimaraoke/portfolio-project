61eb9a80cc33ac6ae22cc8b2d05f0bc77b53519c
Marvellous chimaraoke <rokemarvellous@gmail.com>
2021-01-19 18:46:06 +0100

    removed image fields from models

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=255)
     publish_date = models.DateTimeField()
     body = models.TextField()
-    image = models.ImageField(upload_to='images/')
+    
 
     def __str__(self):
         return self.title
--- jobs/models.py
+++ jobs/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Job(models.Model):
-    image = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=200, default=0)
     summary = models.CharField(max_length=400)
     URL = models.URLField(max_length=200, default=0)
