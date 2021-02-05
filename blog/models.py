bb5a91a3ab85b242029f6cf610714337cf7452a2
Marvellous chimaraoke <rokemarvellous@gmail.com>
2021-02-05 22:12:07 +0100

    fixed tinymce issue

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ from tinymce.models import HTMLField
 class Blog(models.Model):
     title = models.CharField(max_length=255)
     publish_date = models.DateTimeField()
-    body = models.HTMLField()
+    body = HTMLField()
     
 
     def __str__(self):
